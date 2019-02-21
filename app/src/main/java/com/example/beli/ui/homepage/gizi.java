8a4067e4b806501c253e63f50b33ca45adfc0187
Fadhriga Bestari <fadhriga.bestari@gmail.com>
2019-02-21 23:22:13 +0700

    Initial commit for gizi fragment

---
+++ app/src/main/java/com/example/beli/ui/homepage/gizi.java
@@ -0,0 +1,30 @@
+package com.example.beli.ui.homepage;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.beli.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class gizi extends Fragment {
+
+
+    public gizi() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gizi, container, false);
+    }
+
+}
