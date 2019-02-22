ed06c7c57ff593a331b3ab4a6ff9bab35cb9a4c3
azkanab <azkanabilah@gmail.com>
2019-02-22 21:47:01 +0700

    Merging

--- app/src/main/java/com/example/beli/MainActivity.java
+++ app/src/main/java/com/example/beli/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.beli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    public void checkHeartRate(View view) {
+        String message = "Place your finger on the heartrate sensor";
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, CheckHeartRateActivity.class);
+        startActivity(intent);
+    }
+
 }
